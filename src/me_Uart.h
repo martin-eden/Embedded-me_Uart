545c73e9e4df35017f6aab5da7cebc41b970e26e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-29 17:04:02 +0200

    [.^] Interface code comments.

--- src/me_Uart.h
+++ src/me_Uart.h
@@ -11,8 +11,17 @@
 
 namespace me_Uart
 {
+  // [Core] Set-up for given speed (no parity, 8 data bits, 1 stop bit)
+  TBool Init(TUint_4 Speed);
+
+  // [Core] Get byte (if we has one)
+  TBool GetByte(TUint_1 * Value);
+
+  // [Core] Send byte
+  void SendByte(TUint_1 Value);
+
   /*
-    Typical UART connection speeds
+    [Convenience] Typical UART connection speeds
 
     Here are only values that we consider practical in 2025.
   */
@@ -25,16 +34,7 @@ namespace me_Uart
     Speed_1M_Bps = 1000000,
     Speed_2M_Bps = 2000000;
 
-  // Set-up for given speed (no parity, 8 data bits, 1 stop bit)
-  TBool Init(TUint_4 Speed);
-
-  // Get byte (if we has one)
-  TBool GetByte(TUint_1 * Value);
-
-  // Send byte
-  void SendByte(TUint_1 Value);
-
-  // Wait byte. Maybe forever
+  // [Convenience] Wait byte. Maybe forever
   void WaitByte(TUint_1 * Value);
 }
 
